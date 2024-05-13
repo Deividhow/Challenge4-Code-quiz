838a379d417a9a8933f49185f8eefea78b500ff9
SERGIO HOW <vvdoxi@gmail.com>
2024-05-12 23:47:26 -0400

    added questions and answers and adjusted placment

---
+++ assets/js/questionData.js
@@ -0,0 +1,27 @@
+var questionData = [
+  {
+    title:"Commonly used data types DO NOT include:",
+    choices:["1.strings","2.booleans","3.alerts","4.numbers"],
+    solution:"C3"
+  },
+  {
+    title:"The condition in an if/else statement is enclosed with",
+    choices:["1.quotes","2.curly brackets","3.parentheses","4.square brackets"],
+    solution:"C1"
+  },
+  {
+    title:"Arrays in JavaScript can be used to store  .",
+    choices:["1. numbers and strings","2. other arrays","3.booleans","4.all of the above"],
+    solution:"C4"
+  },
+  {
+    title:"string values must be enclosed within  when being assigned to variables.",
+    choices:["1. commas","2.curly brackets","3.quotes","4.parentheses"],
+    solution:"C2"
+  },
+  {
+    title:"A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices:["1.javascript","2.terminal/bash","3.for loops","4.console log"],
+    solution:"C1"
+  }
+]
